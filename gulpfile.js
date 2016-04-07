ae1b9188750cdf07e2de6e8af55e681605977920
Leon Magee <leonmagee@hotmail.com>
2016-04-07 09:33:52 -0700

    everything ported over

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var util = require('gulp-util');
-var livereload = require('gulp-livereload');
+//var livereload = require('gulp-livereload');
 var runSequence = require('run-sequence');
 var inject = require('gulp-inject');
 
@@ -25,17 +25,22 @@ gulp.task('default', function () {
  */
 gulp.task('index', function () {
     var tmp_src = [
-        './bower_components/foundation/css/foundation.min.css',
-        './bower_components/fontawesome/css/font-awesome.min.css',
-        './assets/css/dbz.min.css',
-        './bower_components/angular/angular.min.js',
+        './www/lib/foundation/css/foundation.min.css',
+        './www/lib/fontawesome/css/font-awesome.min.css',
+        './www/css/dbz.min.css',
+        './www/lib/angular/angular.min.js',
         //'//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js',
-        './bower_components/ui-router/release/angular-ui-router.min.js',
-        './app/app.js',
+        './www/lib/ui-router/release/angular-ui-router.min.js',
+        './www/js/app.js',
     ];
-    return gulp.src('./build/index.html')
-        .pipe(inject(gulp.src(tmp_src)))
-        .pipe(gulp.dest('./'));
+    return gulp.src('www/build/index.html')
+        .pipe(inject(gulp.src(tmp_src,{read: false}),
+            {
+                ignorePath: 'www',
+                addRootSlash: false
+            }
+        ))
+        .pipe(gulp.dest('www/'));
 });
 
 gulp.task('index-report', function () {
@@ -46,12 +51,12 @@ gulp.task('index-report', function () {
  * Compile SCSS
  */
 gulp.task('sass', function () {
-    gulp.src(['assets/scss/import.scss'])
+    gulp.src(['scss/import.scss'])
         .pipe(sass({style: 'compressed', errLogToConsole: true}))
         .pipe(rename('dbz.min.css'))
         .pipe(minifycss())
-        .pipe(gulp.dest('assets/css/'))
-        .pipe(livereload());
+        .pipe(gulp.dest('www/css/'));
+        //.pipe(livereload());
     util.log(util.colors.yellow('Kamehameha!'));
 });
 
@@ -74,11 +79,11 @@ gulp.task('watch', function () {
         util.log(util.colors.green('JS file changed' + ' (' + file.path + ')'));
     });
 
-    gulp.watch('build/index.html').on('change', function(file) {
+    gulp.watch('www/build/index.html').on('change', function(file) {
         runSequence('index', 'index-report');
         //server.changed(file.path);
     });
 
-    gulp.watch('assets/scss/**/*.scss', ['sass']);
+    gulp.watch('scss/**/*.scss', ['sass']);
 });
 
